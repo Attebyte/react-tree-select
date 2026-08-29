46cbc8fe593b2c8085c7d53d4b4ce7cfa5c8f0ac
Tristan <trattebery@outlook.com>
2026-08-29 15:16:00 -0500

    prepare for cloudflare pages hosting

--- vite.site.config.ts
+++ vite.site.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Copy public/_redirects into hosting/ for Cloudflare Pages SPA fallback
+  publicDir: 'public',
   build: {
-    copyPublicDir: false,
+    copyPublicDir: true,
     outDir: 'hosting'
   }
 })
