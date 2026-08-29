10be93cbe704bbf7526f30951632ce0143a9b7e7
Tristan <trattebery@outlook.com>
2026-08-29 15:41:44 -0500

    remove redirects

--- vite.site.config.ts
+++ vite.site.config.ts
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Copy public/_redirects into hosting/ for Cloudflare Pages SPA fallback
-  publicDir: 'public',
   build: {
-    copyPublicDir: true,
+    copyPublicDir: false,
     outDir: 'hosting'
   }
 })
